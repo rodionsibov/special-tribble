4d2e0f7fe3041556f99d947d646ddd48b1d3a69e
Rodion Sibov <rodionsibov@gmail.com>
2021-05-03 17:27:08 +0300

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Weather App</h1>
+    <div class="enter-city">
+      <input
+        v-model="data.city"
+        @keyup.enter="getWeather"
+        type="text"
+        placeholder="Enter a city"
+      />
+      <div v-if="data.weather" class="weather">
+        <h1>
+          {{ Math.round(data.weather.main.temp - 273.15) }}
+          &deg;
+        </h1>
+        <h2>
+          {{ data.weather.weather[0].main }}
+        </h2>
+        <h3>
+          {{ data.weather.weather[0].description }}
+        </h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const data = reactive({
+      city: "",
+      weather: null,
+    });
+
+    const apiUrl = "http://localhost:3000/";
+
+    const getWeather = async () => {
+      const res = await fetch(`${apiUrl}?q=${data.city}`);
+      const dataRes = await res.json();
+      data.weather = dataRes;
+    };
+
+    return {
+      data,
+      getWeather,
+    };
+  },
+};
 </script>
+
+<style lang="sass">
+.enter-city
+  input
+    font-size: 40px
+.weather
+  margin-top: 10px
+  h1
+    font-size: 80px
+  h1, h2, h3
+    margin: 0
+</style>
